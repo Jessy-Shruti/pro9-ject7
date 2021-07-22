2abd6f4aa90fc0c48abb0daa7fef2f3f8800dd74
Jessy-Shruti <82816816+Jessy-Shruti@users.noreply.github.com>
2021-07-22 17:09:21 +0530

    Add files via upload

---
+++ game_choice.js
@@ -0,0 +1,25 @@
+function carRace(){
+    document.getElementById("rapirace").value;
+    window.location = "car.html";
+}
+
+function mineCraft(){ 
+    document.getElementById("minecraft").value;
+    window.location = "minecraft.html";
+}
+
+function hitGoal(){
+    document.getElementById("hit_goal").value;
+    window.location = "hitthegoal.html";
+}
+
+function monekeyWalk(){
+    document.getElementById("walkmonkey").value;
+    window.location = "monkeywalk.html";
+}
+
+function moveRover(){
+    document.getElementById("moverover").value;
+
+    window.location = "mars.html";
+}
---
+++ main.js
@@ -0,0 +1,13 @@
+function logIn() {
+
+   document.getElementById("login_in").value;
+
+    window.location = "game_choice.html";
+}
+
+function signUp() {
+
+    document.getElementById("signin_up").value;
+
+    window.location = "game_choice.html";
+}
